127cd1ccfd5c1704ba3c70c754e6a58c90120d15
anuch4rt <anatthayanon@email.com>
2025-10-09 02:59:27 +0700

    Add 'Notify'in quasar.comf.js

--- quasar.config.js
+++ quasar.config.js
@@ -74,7 +74,10 @@ export default defineConfig((ctx) => {
 
     // https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#framework
     framework: {
-      config: {},
+    plugins: [
+      'Notify'
+    ],
+  
 
       // iconSet: 'material-icons', // Quasar icon set
       // lang: 'en-US', // Quasar language pack
